8e065163aec7cb49116073a528675898eaf0bac2
Pechenuxa1 <v.tatarintsev@g.nsu.ru>
2023-09-08 21:08:52 +0700

    modified JavaTask3

--- JavaTask3/src/Task3.java
+++ JavaTask3/src/Task3.java
@@ -1,59 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Task3 {
-  static String[] strings1 = new String[3];
-  static String[] strings2 = new String[3];
-  static String[] strings3 = new String[3];
-  static String[] strings4 = new String[3];
+  static ArrayList<String> strings1 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+  static ArrayList<String> strings2 = new ArrayList<>(Arrays.asList("d", "e", "f"));
+  static ArrayList<String> strings3 = new ArrayList<>(Arrays.asList("g", "h", "i"));
+  static ArrayList<String> strings4 = new ArrayList<>(Arrays.asList("j", "k", "l"));
 
   public static void main(String[] args) {
-    ArrayList<Runnable> tasks = new ArrayList<>();
-
-    stringsInitializer();
-    tasksInitializer(tasks);
-    for (int i = 0; i < 4; i++) {
-      Thread childThread = new Thread(tasks.get(i));
+    ArrayList<Thread> childThreads = new ArrayList<>();
+    childThreads.add(new Thread(new Worker(strings1)));
+    childThreads.add(new Thread(new Worker(strings2)));
+    childThreads.add(new Thread(new Worker(strings3)));
+    childThreads.add(new Thread(new Worker(strings4)));
+    for (Thread childThread : childThreads) {
       childThread.start();
     }
   }
 
-  private static void print(String[] args) {
-    for (int i = 0; i < 3; i++) {
-      System.out.print(args[i]);
+  public static class Worker implements Runnable {
+
+    ArrayList<String> strings;
+
+    Worker(ArrayList<String> strings) {
+      this.strings = strings;
+    }
+
+    @Override
+    public void run() {
+      print(strings);
+    }
+  }
+
+  private static void print(ArrayList<String> strings) {
+    for (String letter : strings) {
+      System.out.print(letter + " ");
     }
     System.out.println();
-  }
-
-  private static void tasksInitializer(ArrayList<Runnable> tasks) {
-    tasks.add(() -> {
-      print(strings1);
-    });
-    tasks.add(() -> {
-      print(strings2);
-    });
-    tasks.add(() -> {
-      print(strings3);
-    });
-    tasks.add(() -> {
-      print(strings4);
-    });
-  }
-
-  private static void stringsInitializer() {
-    strings1[0] = "a";
-    strings1[1] = "b";
-    strings1[2] = "c";
-
-    strings2[0] = "d";
-    strings2[1] = "e";
-    strings2[2] = "f";
-
-    strings3[0] = "g";
-    strings3[1] = "h";
-    strings3[2] = "i";
-
-    strings4[0] = "j";
-    strings4[1] = "k";
-    strings4[2] = "l";
   }
 }
