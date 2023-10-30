81395c2b36a247aa8a32775b2768ba281d3cd349
Pechenuxa1 <v.tatarintsev@g.nsu.ru>
2023-10-30 17:51:20 +0700

    finish JavaTask16

--- JavaTask16/src/main/java/org/example/Main.java
+++ JavaTask16/src/main/java/org/example/Main.java
@@ -7,16 +7,17 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Terminal
-        URL url = new URL("http://lib.ru");
+    public static void main(String[] args) throws IOException {
+        URL url = new URL(args[0]);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         StringBuilder response = new StringBuilder();
         String inputLine;
+        Scanner scanner = new Scanner(System.in);
         int lines = 0;
         int pageSize = 25;
         while ((inputLine = in.readLine()) != null) {
@@ -27,13 +28,14 @@ public class Main {
                 response.setLength(0);
                 lines = 0;
                 System.out.println("Press space to scroll down.");
-                char input = (char) System.in.read();
-                if (input != ' ') {
+                String input = scanner.nextLine();
+                System.out.println(input);
+                if (!input.equals(" ")) {
                     break;
                 }
             }
-            in.close();
-            connection.disconnect();
         }
+        in.close();
+        connection.disconnect();
     }
 }
