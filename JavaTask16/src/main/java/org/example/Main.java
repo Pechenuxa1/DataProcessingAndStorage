c4cbfb377d83ed5d53bc0a0dc0ce6167e0f9f391
Pechenuxa1 <v.tatarintsev@g.nsu.ru>
2023-11-08 22:43:17 +0700

    divided into two threads

--- JavaTask16/src/main/java/org/example/Main.java
+++ JavaTask16/src/main/java/org/example/Main.java
@@ -7,35 +7,65 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         URL url = new URL(args[0]);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String inputLine;
+        BlockingQueue<String> response = new LinkedBlockingQueue<>();
         Scanner scanner = new Scanner(System.in);
+
+        Reader reader = new Reader(in, response);
+        Thread readThread = new Thread(reader);
+        readThread.start();
+
+        String output;
         int lines = 0;
         int pageSize = 25;
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine).append("\n");
+        while (!(output = response.take()).equals("stop output")) {
+            System.out.println(output);
             lines += 1;
-            if (lines == pageSize) {
-                System.out.println(response);
-                response.setLength(0);
-                lines = 0;
+            if(lines == pageSize) {
                 System.out.println("Press space to scroll down.");
                 String input = scanner.nextLine();
-                System.out.println(input);
                 if (!input.equals(" ")) {
                     break;
                 }
+                lines = 0;
             }
         }
-        in.close();
-        connection.disconnect();
+    }
+
+    private static class Reader implements Runnable {
+        BufferedReader in;
+        BlockingQueue response;
+        Reader(BufferedReader in, BlockingQueue response) {
+            this.in = in;
+            this.response = response;
+        }
+
+        @Override
+        public void run() {
+            String inputLine;
+            while (true) {
+                try {
+                    if ((inputLine = in.readLine()) == null) {
+                        response.add("stop output");
+                        break;
+                    }
+                    response.add(inputLine);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
